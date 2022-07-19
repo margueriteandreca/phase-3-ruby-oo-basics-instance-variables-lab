ebd625083c5687c32e7fbe7b11172ba205fdfbc4
+ <+>
2022-07-19 13:24:56 -0400

    commit tree d8a233360ac5285b7b23e43e352a400e1b6606f3

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    # setter 
+    def name=(dog_name) #takes dog's name arugment, assigns it to variable
+        @this_dogs_name = dog_name
+    end
+
+    #getter
+    def name #method reports the name when called as attribute of an instance
+        @this_dogs_name
+    end
 
 end
+
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+lassie.name #=> "Lassie"
